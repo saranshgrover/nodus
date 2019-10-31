4384cf35e796c376bf86fe8599949e5c4c414708
saranshgrover <ycubiksrube@gmail.com>
2019-10-30 23:32:06 -0400

    Add getMe() and getUpcomingCompetitions()

--- src/server/auth.js
+++ src/server/auth.js
@@ -64,7 +64,7 @@ export const signIn = () => {
     client_id: WCA_OAUTH_CLIENT_ID,
     response_type: 'token',
     redirect_uri: oauthRedirectUri(),
-    scope: 'manage_competitions',
+    scope: 'public manage_competitions dob email',
   });
   window.location = `${WCA_ORIGIN}/oauth/authorize?${params.toString()}`;
 };
--- src/server/wca-api.js
+++ src/server/wca-api.js
@@ -1,29 +1,33 @@
   
 import { WCA_ORIGIN } from './wca-env';
 import { wcaAccessToken} from './auth'
+import moment from 'moment';
 
 export const getWcif = (competitionId) =>
   wcaApiFetch( `/competitions/${competitionId}/wcif`);
 
-export const getMyCompetitions = () => {
-    
-}
+  export const getMyUpcomingCompetitions = () => {
+    const today = moment().startOf('day');
+    const params = new URLSearchParams({
+      managed_by_me: true,
+      start: today.toISOString(),
+    });
+    return wcaApiFetch(`/competitions?${params.toString()}`);
+  };
 
-const wcaApiFetch = ( path, fetchOptions = {}) => {
-    const baseApiUrl = `${WCA_ORIGIN}/api/v0`;
+  export const getMe = () => wcaApiFetch( `/me`);
   
-    return fetch(
-      `${baseApiUrl}${path}`,
-      Object.assign({}, fetchOptions, {
+
+const wcaApiFetch = async ( path, fetchOptions = {}) => {
+    const baseApiUrl = `${WCA_ORIGIN}/api/v0`
+    const response = await fetch(`${baseApiUrl}${path}`, Object.assign({}, fetchOptions, {
         headers: new Headers({
-          Authorization: `Bearer ${wcaAccessToken()}`,
-          'Content-Type': 'application/json',
+            Authorization: `Bearer ${wcaAccessToken()}`,
+            'Content-Type': 'application/json',
         }),
-      })
-    )
-      .then(response => {
-        if (!response.ok) throw new Error(response.statusText);
-        return response;
-      })
-      .then(response => response.json());
+    }));
+    if (!response.ok)
+        throw new Error(response.statusText);
+    const response_1 = response;
+    return await response_1.json();
   };
