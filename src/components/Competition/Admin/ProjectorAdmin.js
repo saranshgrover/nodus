2cea9b814570c5ed72bc9488a265242654285fb6
saranshgrover <ycubiksrube@gmail.com>
2019-11-18 16:22:29 -0500

    bug fixes

--- src/components/Competition/Admin/ProjectorAdmin.js
+++ src/components/Competition/Admin/ProjectorAdmin.js
@@ -1,83 +1,109 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions'
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 import Button from '@material-ui/core/Button'
 import FormGroup from '@material-ui/core/FormGroup'
 import TextField from '@material-ui/core/TextField'
-import {getRoomColors} from '../../../server/wcif'
+import { getRoomColors } from '../../../server/wcif'
 import { Typography } from '@material-ui/core'
-import Divider from '@material-ui/core/Divider';
-import {makeStyles} from '@material-ui/core/styles'
+import Divider from '@material-ui/core/Divider'
+import { makeStyles } from '@material-ui/core/styles'
 import ButtonGroup from '@material-ui/core/ButtonGroup'
-import {mapIn} from '../../../server/tools'
+import { mapIn } from '../../../server/tools'
 import ProjectorRoomAdmin from './ProjectorRoomAdmin'
 /*
 TODO: Implement A COLOR PICKER for rooms
 */
 
-const useStyles = makeStyles(theme=>({
-    root: {
-      width: '100%',
-    },
-    title: {
-        margin: theme.spacing(3),
-        textAlign:'center',
-    },
-    group: {
-        justifyContent: 'center',
-    },
-    link: {
-        justifyContent:'center',
-        maxWidth: 200,
-    },
-  }));
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%'
+  },
+  title: {
+    margin: theme.spacing(3),
+    textAlign: 'center'
+  },
+  group: {
+    justifyContent: 'center'
+  },
+  link: {
+    justifyContent: 'center',
+    maxWidth: 200
+  }
+}))
 
-export default function ProjectorAdmin({wcif, setWcif, updateProjectorConfig,venues,id}) {
-    const projectorConfigUpdate = newRoom => {
-        console.log(newRoom)
-        setWcif(
-            mapIn(wcif, ['schedule', 'venues'], venue =>
-                mapIn(venue, ['rooms'], room =>
-                room.id === newRoom.id ? newRoom : room
-                    )
-            )
+export default function ProjectorAdmin({
+  wcif,
+  setWcif,
+  updateProjectorConfig,
+  venues,
+  id
+}) {
+  const projectorConfigUpdate = newRoom => {
+    console.log(newRoom)
+    setWcif(
+      mapIn(wcif, ['schedule', 'venues'], venue =>
+        mapIn(venue, ['rooms'], room =>
+          room.id === newRoom.id ? newRoom : room
         )
-    }
-    const saveWcif = () => {
-        updateProjectorConfig()
-    }
-    const classes = useStyles();
-    return (
-    <>
-        <ExpansionPanelDetails>
-            <FormGroup>
-                {venues.map(venue => venue.rooms.map(room =>
-                    <ProjectorRoomAdmin
-                    key={room.id}
-                    room={room}
-                    onRoomChange={projectorConfigUpdate}
-                    /> 
-                    ))}
-            <Typography className={classes.title} variant='h6' gutterBottom>Link to Projector Screens</Typography>
-            <ButtonGroup className={classes.group}>
-                <Button className={classes.link} variant="outlined" href={`/live/${id}/all`} >
-                    Slideshow
-                </Button>
-                <Button className={classes.link} variant="outlined"  href={`/live/${id}/pin`} >
-                    Pinned Messages
-                </Button>
-                {venues.map(venue =>venue.rooms.map(room=> 
-                    <Button className={classes.link} key={room.id} variant='outlined' href={`/live/${room.id}`}>
-                        {room.name}
-                    </Button>
-                    ))}
-            </ButtonGroup>
-            </FormGroup>
-        </ExpansionPanelDetails>
-        <ExpansionPanelActions>
-            <Button size="small" color="primary" onClick={()=>saveWcif()}>Save</Button>
-        </ExpansionPanelActions>
-    </>
-        
+      )
     )
+  }
+  const saveWcif = () => {
+    updateProjectorConfig()
+  }
+  const classes = useStyles()
+  return (
+    <>
+      <ExpansionPanelDetails>
+        <FormGroup>
+          {venues.map(venue =>
+            venue.rooms.map(room => (
+              <ProjectorRoomAdmin
+                key={room.id}
+                room={room}
+                onRoomChange={projectorConfigUpdate}
+              />
+            ))
+          )}
+          <Typography className={classes.title} variant='h6' gutterBottom>
+            Link to Projector Screens
+          </Typography>
+          <ButtonGroup className={classes.group}>
+            <Button
+              className={classes.link}
+              variant='outlined'
+              href={`/project/${id}/all`}
+            >
+              Slideshow
+            </Button>
+            <Button
+              className={classes.link}
+              variant='outlined'
+              href={`/project/${id}/pin`}
+            >
+              Pinned Messages
+            </Button>
+            {venues.map(venue =>
+              venue.rooms.map(room => (
+                <Button
+                  className={classes.link}
+                  key={room.id}
+                  variant='outlined'
+                  href={`/project/${room.id}`}
+                >
+                  {room.name}
+                </Button>
+              ))
+            )}
+          </ButtonGroup>
+        </FormGroup>
+      </ExpansionPanelDetails>
+      <ExpansionPanelActions>
+        <Button size='small' color='primary' onClick={() => saveWcif()}>
+          Save
+        </Button>
+      </ExpansionPanelActions>
+    </>
+  )
 }
--- src/components/Projector/Projector.js
+++ src/components/Projector/Projector.js
@@ -1,5 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Dialog from '@material-ui/core/Dialog'
+import Slide from '@material-ui/core/Slide'
+import { getWcif } from '../../server/wca-api'
+import Error from '../common/Error'
+import { LinearProgress, CircularProgress } from '@material-ui/core'
 
-export default function Projector() {
-  return <div>PROJECTOR</div>
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction='up' ref={ref} {...props} />
+})
+
+export default function FullScreenDialog({ match }) {
+  const [loading, setLoading] = useState(true)
+  const [wcif, setWcif] = useState(null)
+  const [error, setError] = useState(null)
+  const getScreen = () => {
+    return <div>HELLO</div>
+  }
+  useEffect(() => {
+    getWcif(match.params.compId)
+      .then(wcif => setWcif(wcif))
+      .catch(err => setError(err.message))
+    setLoading(false)
+  }, [match.params.compId])
+  return (
+    <div>
+      {loading && <LinearProgress />}
+      {error && <Error message={error} />}
+      {!loading && wcif && (
+        <Dialog fullScreen open={true} TransitionComponent={Transition}>
+          {getScreen()}
+        </Dialog>
+      )}
+    </div>
+  )
 }
