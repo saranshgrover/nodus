ffa413bd948d6224ce148e759a109d0d99060d59
saranshgrover <ycubiksrube@gmail.com>
2019-11-30 21:02:41 -0500

    Refactor Competition & Dashboard

--- src/components/Competition/Competition.js
+++ src/components/Competition/Competition.js
@@ -1,14 +1,10 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Redirect, Route } from 'react-router-dom'
 import Notifications from './Notifications/Notifications'
 import Groups from './Groups/Groups'
 import Overview from './Overview/Overview'
 import Error from '../common/Error'
 import Admin from './Admin/Admin'
-
-import LinearProgress from '@material-ui/core/LinearProgress'
-
-import { getWcif, getWcifPublic } from '../../server/wca-api'
 import { isExtensionSetup } from '../../server/wcif'
 import {
   getMyAssignmentsInOrder,
@@ -16,104 +12,65 @@ import {
 } from './Overview/OverviewLogic'
 import MySnackbar from '../common/MySnackbar'
 
-class Competition extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      wcif: null,
-      loadingWcif: true,
-      myEvents: [],
-      extensionSetup: false
-    }
-    props.user === 'admin'
-      ? getWcif(this.props.compId).then(res =>
-          this.setState({ wcif: res, loadingWcif: false })
-        )
-      : getWcifPublic(this.props.compId).then(res =>
-          this.setState({ wcif: res, loadingWcif: false })
-        )
-    if (props.user !== 'spectator' && !this.state.loadingWcif) {
-      this.setState({
-        myEvents: getMyEventsInOrder(props.userInfo, this.state.wcif),
-        extensionSetup: isExtensionSetup('GeneralConfig', this.state.wcif)
-      })
-    }
-  }
-  setWcif = newWcif => this.setState({ wcif: newWcif })
-  render() {
-    const { wcif, loadingWcif } = this.state
-    const { user, userInfo, compId } = this.props
-    return (
-      <div>
-        {!loadingWcif && isExtensionSetup('GeneralConfig', wcif) && (
-          <>
-            <Route
-              path={`/competitions/${wcif.id}/overview`}
-              render={props => (
-                <Overview
-                  {...props}
-                  myEvents={getMyEventsInOrder(userInfo, wcif)}
-                  myAssignments={getMyAssignmentsInOrder(
-                    userInfo.me.wca_id,
-                    wcif
-                  )}
-                  wcif={wcif}
-                  user={user}
-                  userInfo={userInfo}
-                />
-              )}
-            />
-            <Route
-              path={`/competitions/${wcif.id}/groups`}
-              render={props => (
-                <Groups
-                  {...props}
-                  user={user}
-                  wcif={wcif}
-                  userInfo={userInfo}
-                />
-              )}
-            />
-            <Route
-              path={`/competitions/${wcif.id}/notifications`}
-              render={props => <Notifications {...props} />}
-            />
-            <Route
-              path={`/competitions/${wcif.id}/admin`}
-              render={props => (
-                <Admin
-                  {...props}
-                  user={user}
-                  wcif={this.state.wcif}
-                  setWcif={this.setWcif}
-                />
-              )}
-            />
-          </>
-        )}
-        {!loadingWcif &&
-          !isExtensionSetup('GeneralConfig', wcif) &&
-          (user === 'admin' ? (
-            <>
-              <Redirect to={`/competitions/${compId}/admin`} />
-              <MySnackbar
-                variant={'error'}
-                message={'Set up Admin information first'}
-              />
-              <Admin
+function Competition({ user, history, localWcif, compId, userInfo }) {
+  const [wcif, setWcif] = useState(localWcif)
+  return (
+    <div>
+      {isExtensionSetup('GeneralConfig', wcif) && (
+        <>
+          <Route
+            path={`/competitions/${wcif.id}/overview`}
+            render={props => (
+              <Overview
+                {...props}
+                myEvents={getMyEventsInOrder(userInfo, wcif)}
+                myAssignments={getMyAssignmentsInOrder(
+                  userInfo.me.wca_id,
+                  wcif
+                )}
+                wcif={wcif}
                 user={user}
-                wcif={this.state.wcif}
-                setWcif={this.setWcif}
+                userInfo={userInfo}
               />
-            </>
-          ) : (
-            <Error
-              message={"😞😞😞 This competition isn't using myComp 😞😞😞 "}
+            )}
+          />
+          <Route
+            path={`/competitions/${wcif.id}/groups`}
+            render={props => (
+              <Groups {...props} user={user} wcif={wcif} userInfo={userInfo} />
+            )}
+          />
+          <Route
+            path={`/competitions/${wcif.id}/notifications`}
+            render={props => <Notifications {...props} />}
+          />
+          <Route
+            path={`/competitions/${wcif.id}/admin`}
+            render={props => (
+              <Admin {...props} user={user} wcif={wcif} setWcif={setWcif} />
+            )}
+          />
+        </>
+      )}
+      {!isExtensionSetup('GeneralConfig', wcif) &&
+        (user === 'admin' ? (
+          <>
+            <Redirect to={`/competitions/${compId}/admin`} />
+            <MySnackbar
+              variant={'error'}
+              message={'Set up Admin information first'}
             />
-          ))}
-        {loadingWcif && <LinearProgress />}
-      </div>
-    )
-  }
+            <Admin user={user} wcif={wcif} setWcif={setWcif} />
+          </>
+        ) : (
+          <Error
+            message={"😞😞😞 This competition isn't using myComp 😞😞😞 "}
+          />
+        ))}
+      <Route exact path={`/competitions/${wcif.id}`}>
+        <Redirect to={`/competitions/${wcif.id}/overview`} />
+      </Route>
+    </div>
+  )
 }
 export default Competition
--- src/components/Competition/Dashboard/Dashboard.js
+++ src/components/Competition/Dashboard/Dashboard.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Drawer from '@material-ui/core/Drawer'
 import Hidden from '@material-ui/core/Hidden'
 import { makeStyles } from '@material-ui/core/styles'
 import DashboardList from './DashboardList'
 import Competition from '../Competition'
+import { getWcifPublic, getWcif } from '../../../server/wca-api'
+import Error from '../../common/Error'
+import { LinearProgress } from '@material-ui/core'
 
 const drawerWidth = 240
 
@@ -53,71 +56,98 @@ function Dashboard({
 }) {
   const classes = useStyles()
   const onItemChange = text => {
-    if (text !== currentComponent) {
-      setCurrentComponent(text)
-      history.push(`/competitions/${match.params.compId}/${text}/`)
-    }
+    setCurrentComponent(text)
+    history.push(`/competitions/${match.params.compId}/${text.toLowerCase()}/`)
   }
   const [currentComponent, setCurrentComponent] = useState('overview')
-  const [user] = useState(
-    managableComps.filter(comp => comp.id === match.params.compId).length > 0
-      ? 'admin'
-      : upcomingComps.filter(comp => comp.id === match.params.compId).length > 0
-      ? 'competitor'
-      : 'spectator'
-  )
+  const [wcif, setWcif] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState('spectator')
+  const [error, setError] = useState(null)
+  useEffect(() => {
+    getWcifPublic(match.params.compId)
+      .then(wcifPublic => {
+        const person = wcifPublic.persons.find(
+          person => person.wcaUserId === userInfo.me.id
+        )
+        if (person && person.roles.length > 0) {
+          getWcif(match.params.compId).then(wcif => {
+            setWcif(wcif)
+            setUser('admin')
+            setLoading(false)
+          })
+        } else if (person && person.roles.length === 0) {
+          setUser('competitor')
+          setWcif(wcifPublic)
+          setLoading(false)
+        }
+      })
+      .catch(err => {
+        setError(err.message)
+        setLoading(false)
+      })
+  }, [match.params.compId, userInfo])
 
   return (
     <div className={classes.root}>
-      <CssBaseline />
-      <nav className={classes.drawer} aria-label='mailbox folders'>
-        <Hidden smUp implementation='css'>
-          <Drawer
-            container={container}
-            variant='temporary'
-            anchor={'left'}
-            open={mobileOpen}
-            onClose={setMobileOpen}
-            classes={{
-              paper: classes.drawerPaper
-            }}
-            ModalProps={{
-              keepMounted: true
-            }}
-          >
-            <div>
-              <div className={classes.toolbar} />
-              <DashboardList user={user} onClick={onItemChange} />
-            </div>
-          </Drawer>
-        </Hidden>
-        <Hidden xsDown implementation='css'>
-          <Drawer
-            classes={{
-              paper: classes.drawerPaper
-            }}
-            variant='permanent'
-            open
-          >
-            <div>
-              <div className={classes.toolbar} />
-              <DashboardList user={user} onClick={onItemChange} />
-            </div>
-          </Drawer>
-        </Hidden>
-      </nav>
-      <main className={classes.content}>
-        {
-          <>
-            <Competition
-              history={history}
-              user={user}
-              compId={match.params.compId}
-              userInfo={userInfo}
-            />
-          </>
-        }
-      </main>
+      {error ? (
+        <Error message={error} />
+      ) : loading ? (
+        <LinearProgress />
+      ) : (
+        <>
+          <CssBaseline />
+          <nav className={classes.drawer} aria-label='mailbox folders'>
+            <Hidden smUp implementation='css'>
+              <Drawer
+                container={container}
+                variant='temporary'
+                anchor={'left'}
+                open={mobileOpen}
+                onClose={setMobileOpen}
+                classes={{
+                  paper: classes.drawerPaper
+                }}
+                ModalProps={{
+                  keepMounted: true
+                }}
+              >
+                <div>
+                  <div className={classes.toolbar} />
+                  <DashboardList user={user} onClick={onItemChange} />
+                </div>
+              </Drawer>
+            </Hidden>
+            <Hidden xsDown implementation='css'>
+              <Drawer
+                classes={{
+                  paper: classes.drawerPaper
+                }}
+                variant='permanent'
+                open
+              >
+                <div>
+                  <div className={classes.toolbar} />
+                  <DashboardList user={user} onClick={onItemChange} />
+                </div>
+              </Drawer>
+            </Hidden>
+          </nav>
+          <main className={classes.content}>
+            {
+              <>
+                <Competition
+                  localWcif={wcif}
+                  history={history}
+                  user={user}
+                  compId={match.params.compId}
+                  userInfo={userInfo}
+                />
+              </>
+            }
+          </main>
+        </>
+      )}
     </div>
   )
 }
