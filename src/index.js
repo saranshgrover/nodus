671309326ea3fc34a509ebcc91890a8744c3fc76
saranshgrover <ycubiksrube@gmail.com>
2020-02-21 14:21:02 -0500

    Make PWA

--- src/index.js
+++ src/index.js
@@ -8,14 +8,17 @@ import { isSignedIn } from './server/auth'
 import { initializeAuth } from './server/auth'
 initializeAuth()
 if (isSignedIn()) {
-  getMe().then(user => {
-    ReactDOM.render(<App userInfo={user} />, document.getElementById('root'))
-  })
+	getMe().then(user => {
+		ReactDOM.render(
+			<App userInfo={user} />,
+			document.getElementById('root')
+		)
+	})
 } else {
-  ReactDOM.render(<App userInfo={{}} />, document.getElementById('root'))
+	ReactDOM.render(<App userInfo={{}} />, document.getElementById('root'))
 }
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.register()
