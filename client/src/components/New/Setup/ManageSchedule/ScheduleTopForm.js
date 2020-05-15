587ad3b6c10a17ce2986aa1f78fe24da82d67ae0
Isaiah Galarza <isaiahgala99@gmail.com>
2020-05-14 21:49:44 -0400

    Adjusted Schedule page to handle timezones.

---
+++ client/src/components/New/Setup/ManageSchedule/ScheduleTopForm.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import DateFnsUtils from "@date-io/date-fns";
+import {
+	KeyboardDatePicker,
+	MuiPickersUtilsProvider,
+} from "@material-ui/pickers";
+
+export default function ScheduleTopForm({
+	date,
+	handleDateChange,
+	numberOfDays,
+	handleNumberOfDays,
+}) {
+	const [localDate, setLocalDate] = useState(date);
+	const [localNumDays, setLocalNumDays] = useState(numberOfDays);
+	return (
+		<>
+			<Grid item>
+				<MuiPickersUtilsProvider utils={DateFnsUtils}>
+					<KeyboardDatePicker
+						disableToolbar
+						variant='inline'
+						format='MM/dd/yyyy'
+						// margin="normal"
+						fullWidth
+						value={`${localDate}T00:00:00`}
+						label='Start Date'
+						name='startDate'
+						onChange={handleDateChange}
+					/>
+				</MuiPickersUtilsProvider>
+			</Grid>
+			<Grid item>
+				<TextField
+					fullWidth
+					error={localNumDays <= 0}
+					helperText='This value must be greater than 0'
+					value={localNumDays}
+					label='Number of days'
+					name='numberOfDays'
+					type='number'
+					onChange={(e) => setLocalNumDays(e.target.value)}
+				/>
+				<Button
+					variant='outlined'
+					onClick={() => {
+						handleNumberOfDays(localNumDays);
+					}}
+				>
+					Set
+				</Button>
+			</Grid>
+		</>
+	);
+}
--- client/src/components/New/Setup/ScheduleSetup.js
+++ client/src/components/New/Setup/ScheduleSetup.js
@@ -1,139 +1,133 @@
-import React, { useState, useEffect } from 'react'
-import gql from 'graphql-tag'
-import { useQuery, useMutation } from '@apollo/react-hooks'
-import LinearProgress from '@material-ui/core/LinearProgress'
-import TextField from '@material-ui/core/TextField'
-import Grid from '@material-ui/core/Grid'
-import StepActions from './StepActions'
-import DateFnsUtils from '@date-io/date-fns'
-import { KeyboardDatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers'
-import CompetitionCalendar from './CompetitionCalendar'
+import React, { useState, useEffect } from "react";
+import gql from "graphql-tag";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
+import StepActions from "./StepActions";
+import CompetitionCalendar from "./ManageSchedule/CompetitionCalendar";
+import ScheduleTopForm from "./ManageSchedule/ScheduleTopForm";
+import moment from "moment-timezone";
 
 const COMPETITION_SCHEDULE_QUERY = gql`
-    query getWcifById($id: String!) {
-        getWcifById(_id: $id) {
-            schedule {
-                startDate
-                numberOfDays
-                venues {
-                    id
-                    name
-                    timezone
-                    rooms {
-                        id
-                        name
-                        color
-                        activities {
-                            id
-                            name
-                            activityCode
-                            startTime
-                            endTime
-                            scrambleSetId
-                        }
-                    }
-                }
-            }
-        }
-    }
-`
+	query getWcifById($id: String!) {
+		getWcifById(_id: $id) {
+			schedule {
+				startDate
+				numberOfDays
+				venues {
+					id
+					name
+					timezone
+					rooms {
+						id
+						name
+						color
+						activities {
+							id
+							name
+							activityCode
+							startTime
+							endTime
+							scrambleSetId
+						}
+					}
+				}
+			}
+		}
+	}
+`;
 const UPDATE_COMPETITION_SCHEDULE_MUTATION = gql`
-    mutation updateWcifSchedule($id: String!, $schedule: ScheduleInput!) {
-        updateWcifSchedule(_id: $id, schedule: $schedule) {
-            id
-        }
-    }
-`
+	mutation updateWcifSchedule($id: String!, $schedule: ScheduleInput!) {
+		updateWcifSchedule(_id: $id, schedule: $schedule) {
+			id
+		}
+	}
+`;
 
-export default function ScheduleSetup({ id, handleReset, onComplete, handleBack }) {
-    const [localData, setLocalData] = useState(null)
-    const query = useQuery(COMPETITION_SCHEDULE_QUERY, {
-        variables: { id: id },
-    })
-    useEffect(() => {
-        !query.loading && !query.error && setLocalData(query.data.getWcifById)
-        !query.loading && !query.error && console.table(query.data.getWcifById)
-    }, [query.loading, query.error, query.data])
+export default function ScheduleSetup({
+	id,
+	handleReset,
+	onComplete,
+	handleBack,
+}) {
+	const [localData, setLocalData] = useState(null);
+	const query = useQuery(COMPETITION_SCHEDULE_QUERY, {
+		variables: { id: id },
+	});
+	useEffect(() => {
+		!query.loading && !query.error && setLocalData(query.data.getWcifById);
+		!query.loading && !query.error && console.table(query.data.getWcifById);
+	}, [query.loading, query.error, query.data]);
 
-    const [updateWcifSchedule, mutationOptions] = useMutation(UPDATE_COMPETITION_SCHEDULE_MUTATION)
+	const [updateWcifSchedule, mutationOptions] = useMutation(
+		UPDATE_COMPETITION_SCHEDULE_MUTATION
+	);
 
-    const handleComplete = () => {
-        console.log(localData)
-        updateWcifSchedule({
-            variables: { id, schedule: localData.schedule },
-        }).then(() => onComplete())
-    }
+	const handleComplete = () => {
+		console.log(localData);
+		updateWcifSchedule({
+			variables: { id, schedule: localData.schedule },
+		}).then(() => onComplete());
+	};
 
-    if (query.loading || !localData) return <LinearProgress />
-    if (query.error) console.error(query.error)
+	if (query.loading || !localData) return <LinearProgress />;
+	if (query.error) console.error(query.error);
 
-    const handleDateChange = (e) => {
-        setLocalData({ ...localData, schedule: { ...localData.schedule, startDate: e.toISOString() } })
-    }
+	const handleDateChange = (e) => {
+		setLocalData({
+			...localData,
+			schedule: {
+				...localData.schedule,
+				startDate: moment(e).format("YYYY-MM-DD"),
+			},
+		});
+	};
 
-    const handleScheduleChange = ({ target: { name, value } }) => {
-        let v = null
-        if (name === 'startDate') {
-            v = new Date()
-            v.setUTCHours(0, 0, 0, 0)
-            v = v.toISOString()
-        } else {
-            v = isNaN(value) ? value : parseInt(value)
-        }
-        setLocalData({ ...localData, schedule: { ...localData.schedule, [name]: v } })
-    }
-    return (
-        <Grid container direction='column' justify='space-between'>
-            <Grid item>
-                <Grid
-                    container
-                    direction='column'
-                    spacing={2}
-                    justify='center'
-                    xs={12}
-                    alignItems='center'
-                    alignContent='center'
-                    wrap='nowrap'
-                >
-                    <Grid item>
-                        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                            <KeyboardDatePicker
-                                disableToolbar
-                                variant='inline'
-                                format='MM/dd/yyyy'
-                                // margin="normal"
-                                fullWidth
-                                value={new Date(localData.schedule.startDate)}
-                                label='Start Date'
-                                name='startDate'
-                                onChange={handleDateChange}
-                            />
-                        </MuiPickersUtilsProvider>
-                    </Grid>
-                    <Grid item>
-                        <TextField
-                            fullWidth
-                            value={localData.schedule.numberOfDays}
-                            label='Number of days'
-                            name='numberOfDays'
-                            type='number'
-                            onChange={handleScheduleChange}
-                        />
-                    </Grid>
-                    <Grid item></Grid>
-                </Grid>
-            </Grid>
-            <Grid item>
-                <CompetitionCalendar schedule={localData.schedule} updateData={setLocalData} />
-            </Grid>
-            <Grid item>
-                <StepActions
-                    handleBack={handleBack}
-                    loading={mutationOptions.loading}
-                    handleComplete={handleComplete}
-                    handleReset={handleReset}
-                />
-            </Grid>
-        </Grid>
-    )
+	const handleNumberOfDays = (numberOfDays) => {
+		if (isNaN(numberOfDays) || numberOfDays < 1) {
+			return;
+		}
+		setLocalData({
+			...localData,
+			schedule: { ...localData.schedule, numberOfDays },
+		});
+	};
+
+	return (
+		<Grid container direction='column' justify='space-between'>
+			<Grid item>
+				<Grid
+					container
+					direction='column'
+					spacing={2}
+					justify='center'
+					xs={12}
+					alignItems='center'
+					alignContent='center'
+					wrap='nowrap'
+				>
+					<ScheduleTopForm
+						date={localData.schedule.startDate}
+						handleDateChange={handleDateChange}
+						numberOfDays={localData.schedule.numberOfDays}
+						handleNumberOfDays={handleNumberOfDays}
+					/>
+				</Grid>
+			</Grid>
+			<Grid item>
+				<CompetitionCalendar
+					schedule={localData.schedule}
+					updateData={setLocalData}
+				/>
+			</Grid>
+			<Grid item>
+				<StepActions
+					handleBack={handleBack}
+					loading={mutationOptions.loading}
+					handleComplete={handleComplete}
+					handleReset={handleReset}
+				/>
+			</Grid>
+		</Grid>
+	);
 }
