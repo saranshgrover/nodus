491954513337b0f8c344fbf75f449dc825b1b95d
saranshgrover <ycubiksrube@gmail.com>
2020-07-07 22:14:25 -0400

    :sparkles: Add Typescript

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -5,7 +5,7 @@ import UserProvider from '../../contexts/UserContext'
 import { ApolloProvider } from 'react-apollo'
 import { client } from '../../utils/ApolloClient'
 import ThemeProvider from '../../contexts/ThemeProvider'
-import Navigation from '../../components/Navigation/Navigation'
+import Navigation from '../Navigation/Navigation'
 import CssBaseline from '@material-ui/core/CssBaseline'
 
 export default function App() {
--- client/src/components/ErrorFallback/ErrorFallback.tsx
+++ client/src/components/ErrorFallback/ErrorFallback.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import {FallbackProps} from 'react-error-boundary'
+
+
 
 export default function ErrorFallback({
 	error,
 	componentStack,
-	resetErrorBoundary,
-}) {
+	resetErrorBoundary
+}:FallbackProps) {
 	return (
 		<div role='alert'>
 			<p>Something went wrong:</p>
-			<pre>{error.message}</pre>
+			<pre>{error?.message}</pre>
 			<pre>{componentStack}</pre>
 			<button onClick={resetErrorBoundary}>Try again</button>
 		</div>
